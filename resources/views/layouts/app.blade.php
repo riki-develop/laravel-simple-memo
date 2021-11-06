b080515238db50fc81a572dc6481cae641320adb
riki karasawa <fabulous.riki@gmail.com>
2021-11-06 15:50:41 +0900

    メモ作成機能開発_View部分の作成

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -101,7 +101,6 @@
                     </div>
                 </div>
                 <div class="col-md-6 p-0">
-                    右カラム
                     @yield('content')
                 </div>
             </div>
