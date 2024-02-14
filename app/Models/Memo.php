38613b31f82b879bc8ad2c4f27350efebaa67e22
riki-develop <fabulous.riki@gmail.com>
2024-02-14 22:55:37 +0900

    レクチャー48：履歴

--- app/Models/Memo.php
+++ app/Models/Memo.php
@@ -8,4 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class Memo extends Model
 {
     use HasFactory;
+
+    public function getMyMemo() {
+        // リクエストファザードを使ってURLパラメータを取得
+        // 通常useで宣言して使うclassだがバックスラッシュを付ける事で単発使用可になる
+        $query_tag = \Request::query('tag');
+
+        // ========== ベースメソッドを定義 ==========
+        $query = Memo::query()->select('memos.*')
+            ->where('user_id', '=', \Auth::id())
+            ->whereNull('deleted_at')
+            ->orderBy('updated_at', 'DESC');
+        // ========== ベースメソッドを定義 ==========
+
+        if(!empty($query_tag)) {
+            // ▼もしtag_URLクエリパラメータがあればタグで絞り込み
+            $query->leftJoin('memo_tags', 'memo_tags.memo_id', '=', 'memos.id')
+            ->where('memo_tags.tag_id', '=', $query_tag);
+        }
+
+        $memos = $query->get();
+
+        return $memos;
+    }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,32 +27,13 @@ class AppServiceProvider extends ServiceProvider
     {
         // 全てのメソッドが呼ばれる前に先に呼ばれるメソッド
         view()->composer('*', function ($view) {
-
-            // リクエストファザードを使ってURLパラメータを取得
-            // 通常useで宣言して使うclassだがバックスラッシュを付ける事で使用可能になる
-            $query_tag = \Request::query('tag');
-
-            if(!empty($query_tag)) {
-                // ▼もしtag_URLクエリパラメータがあればタグで絞り込み
-                $memos = Memo::select('memos.*')
-                    ->leftJoin('memo_tags', 'memo_tags.memo_id', '=', 'memos.id')
-                    ->where('memo_tags.tag_id', '=', $query_tag)
-                    ->where('user_id', '=', \Auth::id())
-                    ->whereNull('deleted_at')
-                    ->orderBy('updated_at', 'DESC') // ASC=昇順　　DESC=降順
-                    ->get();
-            }else{
-                // ▼tag_URLクエリパラメータが無ければすべて取得
-                $memos = Memo::select('memos.*')
-                    ->where('user_id', '=', \Auth::id())
-                    ->whereNull('deleted_at')
-                    ->orderBy('updated_at', 'DESC') // ASC=昇順　　DESC=降順
-                    ->get();
-            }
-
             /**
-             * タグ絞り込み機能実装に当たり、ここで定義→Viewに渡す
+             * 自分のメモデータ取得は「app/Models/Memo.php」で定義
              */
+            $memo_model = new Memo(); // インスタンス化
+            $memos = $memo_model->getMyMemo();
+
+            // タグ絞り込み機能実装に当たり、ここで定義→Viewに渡す
             $tags = Tag::where('user_id', '=', \Auth::id())
                 ->whereNull('deleted_at')
                 ->orderBy('id', 'DESC')
